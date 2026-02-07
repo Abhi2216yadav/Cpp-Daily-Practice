ea741efdf64cba917617aa0f1a149adf09ddb2e3
Abhi2216yadav <abhay189992@gmail.com>
2026-02-07 12:21:54 +0530

    Day 1: Age program

---
+++ Day-01-Age.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout <<"Enter your age :";
+    cin >> age ;
+    if(age>=18)
+    {
+        cout <<"You can vote\n ";
+    }else{
+        cout <<"You con not vote";
+    }
+    return 0;
+}
