3a4b9319f90cdf7a78206cd1c578fef55361da8f
Abhi2216yadav <abhay189992@gmail.com>
2026-02-10 13:44:47 +0530

    concanate_of_two_str.cpp

---
+++ concanate_of_two_str.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str ="Hello world";
+    string str2 =" Abhay";
+    string str3 = str + str2;
+    cout<<  str3<<endl;
+    return 0;
+}
