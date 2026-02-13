b2ecc9ce19268b28df573f60f9f36831f65c15e3
Abhi2216yadav <abhay189992@gmail.com>
2026-02-13 14:31:31 +0530

    find_max_sum_row_wise.cpp

---
+++ find_max_sum_row_wise.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row , col, i ,j;
+    cout<< "Enter row and col of the matrix : ";
+    cin>> row >> col;
+    int matrix[row][col];
+    cout << "Enter value of the matrix : ";
+    for( i=0 ;i<row ; i++){
+        for(j = 0; j<col ; j++){
+            cin>> matrix[i][j];
+        }
+    }
+    int ans = 0;
+    for( i=0 ;i<row ; i++){
+            int sum =0;
+        for(j = 0; j<col ; j++){
+            sum += matrix[i][j];
+        }
+        cout<< i+1<< " Row sum = "<<sum<<endl;
+       ans = max(sum , ans);
+    }
+
+    cout << "Max Row sum is = " << ans<<endl;
+    return 0;
+}
