59a7314afec8531ef6992923a4d0827f85f5612e
Abhi2216yadav <abhay189992@gmail.com>
2026-02-25 10:26:08 +0530

    square_Pattern.cpp

---
+++ square_Pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n ;
+    int num =1;
+     cout <<"Enter no :";
+    cin >> n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++){
+            cout << num <<"  ";
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
