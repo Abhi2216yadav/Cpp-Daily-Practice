5d283cde24a20a8ceda5902b6ca94c66d6b5ea78
Abhi2216yadav <abhay189992@gmail.com>
2026-03-01 13:25:24 +0530

    string_comparision.cpp

---
+++ string_comparision.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str ="Hello world";
+    string str2 ="Hello world";
+    cout<< (str == str2) <<endl;
+    return 0;
+}
